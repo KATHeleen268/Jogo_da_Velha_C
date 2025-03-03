e4b887d04dd2e7c168e4b351ff38fff67c370ef4
KATHeleen268 <katheleeneduarda123@gmail.com>
2025-03-03 03:05:22 -0300

    Update Jogo_da_velha.c

--- Jogo_da_velha.c
+++ Jogo_da_velha.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+//função que vai ser usada para retornar o tabuleiro vazio
 void tabuleiroJogo(char JV[3][3]){
     for(int i = 0; i < 3; i++) {
        for(int j = 0; j < 3; j++) {
-          printf("%c", JV[i][j]);
+          printf("%c", JV[i][j]);//JV = jogo da velha
           if (j < 2) printf("  |"); 
           
        }
@@ -13,31 +14,53 @@ void tabuleiroJogo(char JV[3][3]){
     printf("\n");
 }
 
+int validandoEscolhaJogador(int escolhalinha, int escolhacoluna, char jogo_da_velha[3][3]){
+       if( escolhalinha >= 0 && escolhalinha < 3 && escolhacoluna >= 0 && escolhacoluna < 3 && jogo_da_velha[escolhalinha][escolhacoluna] == ' '){
+           return 1;
+    } else {
+      printf("resultado inválido!");
+      return 0;
+    }//função que vai validar se pode jogar na linha e coluna se estiver vazia 
+    }
+
 int main()
 {
     printf("Olá, Vamos começar nosso jogo\n");
     
-    int i,j;
+    int i,j, escolhalinha, escolhacoluna;
     char escolha;
-    char jogadores[2] = {'X','O'};
-    char jogo_da_velha[3][3];
+    char jogadores[2] = {'X','O'};//vetor que vai armazenar os valores do jogo_da_velha
+    char jogo_da_velha[3][3];  // matriz do jogo
     
-    for(i = 0; i < 3; i++){
+    for(i = 0; i < 3; i++){ 
         for(j = 0; j < 3; j++){
-            jogo_da_velha[i][j] = ' ';
+            jogo_da_velha[i][j] = ' ';  //fazendo o for da matriz para que tenha linhas e colunas e seja bonitinha e vazia
         }
     }
     
     printf("Escolha se você que ser X ou O:");
-    scanf(" %c", &escolha);
+    scanf(" %c", &escolha); //deixando o jogador/usuário escolher o que vai ser
     
     if(escolha == 'X'){
         printf("Jogador %c, vamos começar!\n", escolha);
-        tabuleiroJogo(jogo_da_velha);
+        tabuleiroJogo(jogo_da_velha); //função sendo testada
     } else {
         printf("Jogador %c, vamos começar!\n", escolha);
-        tabuleiroJogo(jogo_da_velha);
+        tabuleiroJogo(jogo_da_velha); //função sendo testada
     }
+
+    do {
+    printf("Jogador %c, digite a linha e a coluna (0, 1 ou 2):\n", escolha); //pede a escolha do jogador para colocar no local
+    scanf("%d %d", &escolhalinha, &escolhacoluna);//jogador vai digitar sua escolha
+    
+   if (validandoEscolhaJogador(escolhalinha, escolhacoluna, jogo_da_velha)) {
+            jogo_da_velha[escolhalinha][escolhacoluna] = escolha; // testando se funciona 
+            tabuleiroJogo(jogo_da_velha);
+   } else {
+       printf("Inválido,tente novamente\n");
+   }
+    
+    }while(1);
     
     return 0;
 }
